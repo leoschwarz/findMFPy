abdfa44f809b444b80812227ef6b7cb887aa3d46
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-21 17:07:47 +0200

    initial testing

--- tests/test_compiled.py
+++ tests/test_compiled.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from pathlib import Path
+
+import numpy as np
+
 import findmfpy._core as m
 
 
@@ -9,3 +13,16 @@ def test_add():
 
 def test_subtract():
     assert m.subtract(7, 5) == 2
+
+
+def test_pick_peaks():
+    mz_arr, int_arr = np.load(Path(__file__).parent / "data" / "sample.npy")
+    print(mz_arr)
+    print(int_arr)
+
+    mz_peak, int_peak = m.pick_peaks(mz_arr, int_arr)
+
+    print(mz_peak)
+    print(int_arr)
+
+    assert len(mz_peak) == len(mz_arr)
