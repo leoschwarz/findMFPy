83b4202675740abbee48ce9c9f225b8fa8d513d1
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-21 17:07:58 +0200

    initial function

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "base/base/base.h"
 #include "base/base/cumsum.h"
@@ -8,6 +9,18 @@
 #include <boost/test/unit_test.hpp>
 
 int add(int i, int j) { return i + j; }
+
+auto pick_peaks(const std::vector<double> &mz_arr,
+                const std::vector<double> &int_arr) {
+  const double resolution = 10000;
+  std::pair<double, double> massrange =
+      std::make_pair(mz_arr.front(), mz_arr.back());
+  ralab::base::ms::PeakPicker<double, ralab::base::ms::SimplePeakArea> pp(
+      resolution, massrange);
+  pp(mz_arr.begin(), mz_arr.end(), int_arr.begin());
+
+  return std::make_tuple(pp.getPeakMass(), pp.getPeakArea());
+}
 
 namespace py = pybind11;
 
@@ -30,5 +43,9 @@ PYBIND11_MODULE(_core, m) {
   m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
       Subtract two numbers
       Some other explanation about the subtract function.
+  )pbdoc");
+
+  m.def("pick_peaks", &pick_peaks, R"pbdoc(
+      Pick peaks from a spectrum
   )pbdoc");
 }
