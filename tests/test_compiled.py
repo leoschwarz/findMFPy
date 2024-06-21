29b3b3e5e6c17935f0219e437e46d27da111073b
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-21 17:12:41 +0200

    nicer plot

--- tests/test_compiled.py
+++ tests/test_compiled.py
@@ -2,27 +2,19 @@ from __future__ import annotations
 
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 import findmfpy._core as m
 
 
-def test_add():
-    assert m.add(2, 3) == 5
-
-
-def test_subtract():
-    assert m.subtract(7, 5) == 2
-
-
 def test_pick_peaks():
     mz_arr, int_arr = np.load(Path(__file__).parent / "data" / "sample.npy")
-    print(mz_arr)
-    print(int_arr)
-
     mz_peak, int_peak = m.pick_peaks(mz_arr, int_arr)
 
-    print(mz_peak)
-    print(int_arr)
+    plt.plot(mz_arr, int_arr)
+    plt.scatter(mz_peak, int_peak, color="red", marker="x")
+    plt.xlim(1000, 1100)
+    plt.savefig("test_pick_peaks.pdf")
 
     assert len(mz_peak) == len(mz_arr)
