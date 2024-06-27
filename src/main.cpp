67e8fadceee95343c019febe20d3d03c35d60fd4
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-27 16:47:34 +0200

    start refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,44 @@
 #include "base/ms/generatesamplespec.h"
 #include "base/ms/peakpickerqtof.h"
 #include "base/stats/normal.h"
-// #include <boost/test/unit_test.hpp>
 
-// int add(int i, int j) { return i + j; }
+namespace py = pybind11;
+using PeakPicker =
+    ralab::base::ms::PeakPicker<double, ralab::base::ms::SimplePeakArea>;
 
 namespace py = pybind11;
 
-auto pick_peaks(const std::vector<double> &mz_arr,
-                const std::vector<double> &int_arr,
-                const double resolution = 10000, const double width = 2.0,
-                const double int_width = 2.0, const double int_threshold = 10.0,
-                const bool area = true, const uint32_t max_peaks = 0) {
-  std::pair<double, double> massrange =
-      std::make_pair(mz_arr.front(), mz_arr.back());
-  ralab::base::ms::PeakPicker<double, ralab::base::ms::SimplePeakArea> pp(
-      resolution, massrange, width, int_width, int_threshold, area, max_peaks);
-  pp(mz_arr.begin(), mz_arr.end(), int_arr.begin());
+auto pick_peaks_generic(const py::array_t<double> &mz_arr,
+                        const py::array_t<double> &int_arr,
+                        const double resolution, const double width,
+                        const double int_width, const double int_threshold,
+                        const bool area, const uint32_t max_peaks) {
 
-  return std::make_tuple(pp.getPeakMass(), pp.getPeakArea());
-}
-
-auto pick_peaks_np(const py::array_t<double> &mz_arr,
-                   const py::array_t<double> &int_arr,
-                   const double resolution = 10000, const double width = 2.0,
-                   const double int_width = 2.0,
-                   const double int_threshold = 10.0, const bool area = true,
-                   const uint32_t max_peaks = 0) {
   std::vector<double> mz_vec(mz_arr.data(), mz_arr.data() + mz_arr.size());
   std::vector<double> int_vec(int_arr.data(), int_arr.data() + int_arr.size());
 
-  return pick_peaks(mz_vec, int_vec, resolution, width, int_width,
-                    int_threshold, area, max_peaks);
+  std::pair<double, double> massrange =
+      std::make_pair(mz_vec.front(), mz_vec.back());
+  PeakPicker pp(resolution, massrange, width, int_width, int_threshold, area,
+                max_peaks);
+  pp(mz_vec.begin(), mz_vec.end(), int_vec.begin());
+  return pp;
+}
+
+auto pick_peaks(const py::array_t<double> &mz_arr,
+                const py::array_t<double> &int_arr, const double resolution,
+                const double width, const double int_width,
+                const double int_threshold, const bool area,
+                const uint32_t max_peaks) {
+  std::vector<double> mz_vec(mz_arr.data(), mz_arr.data() + mz_arr.size());
+  std::vector<double> int_vec(int_arr.data(), int_arr.data() + int_arr.size());
+
+  std::pair<double, double> massrange =
+      std::make_pair(mz_vec.front(), mz_vec.back());
+  PeakPicker pp(resolution, massrange, width, int_width, int_threshold, area,
+                max_peaks);
+  pp(mz_vec.begin(), mz_vec.end(), int_vec.begin());
+  return std::make_tuple(pp.getPeakMass(), pp.getPeakArea());
 }
 
 PYBIND11_MODULE(_core, m) {
@@ -49,19 +56,7 @@ PYBIND11_MODULE(_core, m) {
          :toctree: _generate
   )pbdoc";
 
-  /* keeping these around as example for now
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-        Some other explanation about the subtract function.
-    )pbdoc");
-  */
-
-  m.def("pick_peaks", &pick_peaks_np, R"pbdoc(
+  m.def("pick_peaks", &pick_peaks, R"pbdoc(
       Pick peaks from a spectrum
   )pbdoc");
 }
