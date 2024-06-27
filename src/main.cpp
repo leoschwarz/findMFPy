46dec91cc301a9f23951e8ab8398e188949d6eb3
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-27 17:15:25 +0200

    add diagnostic method

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,20 @@ auto pick_peaks(const py::array_t<double> &mz_arr,
   return std::make_tuple(pp.getPeakMass(), pp.getPeakArea());
 }
 
+auto pick_peaks_diagnostic(const py::array_t<double> &mz_arr,
+                           const py::array_t<double> &int_arr,
+                           const double resolution, const double width,
+                           const double int_width, const double int_threshold,
+                           const bool area, const uint32_t max_peaks) {
+  auto pp = pick_peaks_generic(mz_arr, int_arr, resolution, width, int_width,
+                               int_threshold, area, max_peaks);
+  //  return std::make_tuple(pp.getPeakMass(), pp.getPeakArea());
+
+  return std::make_tuple(pp.getPeakMass(), pp.getPeakArea(),
+                         pp.getResampledMZ(), pp.getResampledIntensity(),
+                         pp.getSmoothedIntensity());
+};
+
 PYBIND11_MODULE(_core, m) {
   m.doc() = R"pbdoc(
       Pybind11 example plugin
@@ -52,5 +66,9 @@ PYBIND11_MODULE(_core, m) {
 
   m.def("pick_peaks", &pick_peaks, R"pbdoc(
       Pick peaks from a spectrum
+  )pbdoc");
+
+  m.def("pick_peaks_diagnostic", &pick_peaks_diagnostic, R"pbdoc(
+      Pick peaks from a spectrum diagnostic version
   )pbdoc");
 }
