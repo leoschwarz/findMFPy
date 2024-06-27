e0e9e87be0b206d96c7fe9f1f3cca0f81fd36452
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-27 10:58:14 +0200

    add pyi definition

--- src/findmfpy/_core.pyi
+++ src/findmfpy/_core.pyi
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
-# TODO
-# def pick_peaks(
-#    _mz_arr: NDArray[float],
-#    _int_arr: NDArray[float],
-#    _resolution: float = 10000.0,
-#    _width: float = 2.0,
-#    _int_width: float = 2.0,
-#    _int_threshold: float = 10.0,
-#    _area: bool = True,
-#    _max_peaks: int = 0,
-# ) -> tuple[NDArray[float], NDArray[float]]: ...
+import numpy as np
+from numpy.typing import NDArray
+
+def pick_peaks(
+    _mz_arr: NDArray[np.float64],
+    _int_arr: NDArray[np.float64],
+    _resolution: float,
+    _width: float,
+    _int_width: float,
+    _int_threshold: float,
+    _area: bool,
+    _max_peaks: int,
+) -> tuple[NDArray[np.float64], NDArray[np.float64]]: ...
