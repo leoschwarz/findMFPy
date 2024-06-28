d5f5b1c1d88913289aa8ad5b4cd3a3654d77236b
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-06-28 08:32:42 +0200

    formatting

--- tests/test_api.py
+++ tests/test_api.py
@@ -46,6 +46,6 @@ def test_pick_peaks_diagnostic_when_defaults(profile_spectrum: SpectrumType) -> 
     assert len(diagnostics["resampled_mz_arr"]) == len(diagnostics["resample_int_arr"])
     assert len(diagnostics["resampled_mz_arr"]) == len(diagnostics["smoothed_int_arr"])
 
+
 if __name__ == "__main__":
     pytest.main()
-    
